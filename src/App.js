97c8460b81d03db0161cc2779dab51002ccbf285
Jeff "Jack" Aden <jeaden38@gmail.com>
2024-05-30 08:39:21 -0500

    theme light and dark mode

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-
+import { ColorModeContext, useMode} from './theme';
+// CssBaseline-reset css to default, themeprovide-provide ability to pass themes into mui
+import { CssBaseline, ThemeProvider } from '@mui/material';
 
 function App() {
+  //access to theme and colorMode
+  const [theme, colorMode] = useMode();
+
   return (
-    <div className="app">
-      
-    </div>
+    //set up colorContext to access everywhere
+    //ThemeProvider-gives mui access
+    //CssBaseline-reset to css default
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <main className="content"></main>
+        </div>
+        </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
 
