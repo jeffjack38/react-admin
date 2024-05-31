14237e07803844f223cfa4714e3a4fb1d966eb85
Jeff "Jack" Aden <jeaden38@gmail.com>
2024-05-31 17:48:45 -0500

    comment topbar

--- src/scenes/global/Topbar.jsx
+++ src/scenes/global/Topbar.jsx
@@ -9,6 +9,8 @@ import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
 import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
 import SearchIcon from "@mui/icons-material/Search";
 
+
+//Topbar functional component contains the search bar and icons
 const Topbar = () => {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
